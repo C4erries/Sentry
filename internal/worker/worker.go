14e66b8d6e0a854d48c094444a142e30fdeb2da2
c4erries <c4erries@gmail.com>
2025-07-04 16:43:29 +0300

    worker pool

---
+++ internal/worker/worker.go
@@ -0,0 +1,43 @@
+package worker
+
+import (
+	"context"
+	"log"
+	"sync"
+
+	"github.com/c4erries/Sentry/internal/events"
+)
+
+type EventHalndler interface {
+	Process(ctx context.Context, e events.Event) error
+}
+
+func StartPool(ctx context.Context, jobs <-chan events.Event, handler EventHalndler, workerCount int) *sync.WaitGroup {
+	var wg sync.WaitGroup
+
+	for id := 0; id < workerCount; id++ {
+		wg.Add(1)
+		go func(workerID int) {
+			defer wg.Done()
+			log.Printf("[worker-%d] started", workerID)
+			for {
+				select {
+				case <-ctx.Done():
+					log.Printf("[worker-%d] stopping (context canceled)", workerID)
+					return
+
+				case job, ok := <-jobs:
+					if !ok {
+						log.Printf("[worker-%d] stopping (jobs channel closed)", workerID)
+						return
+					}
+					if err := handler.Process(ctx, job); err != nil {
+						log.Printf("[worker-%d] error processing event %v: %v", workerID, job, err)
+					}
+				}
+			}
+		}(id)
+	}
+
+	return &wg
+}
