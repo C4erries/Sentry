029ab3b5c3b5e97394d79e9fc1cfa624c0b84521
c4erries <c4erries@gmail.com>
2025-07-06 18:00:49 +0300

    Patch. anomaly detection + redis. Anomaly reaction -- cmd.

--- cmd/sentry/main.go
+++ cmd/sentry/main.go
@@ -7,22 +7,30 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/c4erries/Sentry/internal/events"
+	"github.com/c4erries/Sentry/internal/alert"
 	"github.com/c4erries/Sentry/internal/handler"
 	"github.com/c4erries/Sentry/internal/kafka"
+	"github.com/c4erries/Sentry/internal/model"
 	"github.com/c4erries/Sentry/internal/worker"
+	go_redis "github.com/redis/go-redis/v9"
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	jobs := make(chan events.Event, 100)
+	redisClient := go_redis.NewClient(&go_redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+	})
 
-	processor, err := handler.NewProcessor()
+	dispatcher := alert.NewDispatcher(&alert.CmdSink{}, 5)
+	go dispatcher.Run(ctx)
+	processor, err := handler.NewProcessor(redisClient, dispatcher)
 	if err != nil {
 		log.Fatalf("cannot create processor: %v", err)
 	}
+
+	jobs := make(chan model.Event, 100)
 	wg := worker.StartPool(ctx, jobs, processor, 5)
 
 	consumer, err := kafka.NewConsumer([]string{"kafka:29092"}, "events_topic", "sentry-core")
@@ -38,6 +46,7 @@ func main() {
 	<-quit
 	cancel()
 	close(jobs)
+	close(dispatcher.Chan)
 	wg.Wait()
 	log.Println("Service stopped.")
 }
--- go.mod
+++ go.mod
@@ -8,8 +8,12 @@ require (
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/google/uuid v1.6.0
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/redis/go-redis/v9 v9.11.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 )
