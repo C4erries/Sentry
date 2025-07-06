621cd99699375c7a1069bf9c56713ada224ebf8f
c4erries <c4erries@gmail.com>
2025-07-06 17:59:38 +0300

    Workers handle

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,27 +4,48 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
-	"github.com/c4erries/Sentry/internal/events"
+	"github.com/c4erries/Sentry/internal/alert"
+	"github.com/c4erries/Sentry/internal/anomaly"
+	"github.com/c4erries/Sentry/internal/model"
+	"github.com/c4erries/Sentry/internal/redis"
 )
 
 type Processor struct {
+	dupGuard   *anomaly.DuplicateGuard
+	registry   *anomaly.DetectorRegistry
+	dispatcher *alert.Dispatcher
 	// db *sql.DB
-	// cache redis.Client
 }
 
-func NewProcessor() (*Processor, error) {
-	return &Processor{}, nil
+func NewProcessor(cache redis.RedisClient, dispatcher *alert.Dispatcher) (*Processor, error) {
+	dupGuard := anomaly.NewDuplicateGuard(cache, 15*time.Second)
+	reg := &anomaly.DetectorRegistry{}
+	reg.Registry(anomaly.NewLoginStormDetector(cache, 15*time.Minute, 5))
+	return &Processor{
+		dupGuard:   dupGuard,
+		registry:   reg,
+		dispatcher: dispatcher,
+	}, nil
 }
 
-func (p *Processor) Process(ctx context.Context, e events.Event) error {
-	log.Printf("Processing [type:%v] [timestamp:%v]", e.EventType, e.Timestamp.String())
-	switch e.EventType {
-	case events.EventLogin:
-		return p.handleLogin(ctx, e)
-	case events.EventTransaction:
-		return p.handleTransaction(ctx, e)
-	default:
-		return fmt.Errorf("can't process event: Unimplemented event type")
+func (p *Processor) Process(ctx context.Context, e model.Event) error {
+	isDup, err := p.dupGuard.IsDuplicate(ctx, e)
+	if err != nil {
+		return fmt.Errorf("dupGuard ISDUPLICATE error: %v", err)
 	}
+
+	if isDup {
+		return nil
+	}
+	alerts := p.registry.ProcessAll(ctx, e)
+	for _, alert := range alerts {
+		select {
+		case p.dispatcher.Chan <- alert:
+		default:
+			log.Panicln("alert channel full, dropped alert:", alert)
+		}
+	}
+	return nil
 }
