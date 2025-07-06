89ba2823daf2b4fa5b1787b30f8c22f9dfc8fbd3
c4erries <c4erries@gmail.com>
2025-07-06 17:58:29 +0300

    NEW PACKAGE "ANOMALY" -- anomaly detection

---
+++ internal/anomaly/anomaly.go
@@ -0,0 +1,38 @@
+package anomaly
+
+import (
+	"context"
+	"log"
+
+	"github.com/c4erries/Sentry/internal/model"
+)
+
+type Detector interface {
+	ID() string
+	Process(ctx context.Context, event model.Event) (*model.Alert, error)
+}
+
+type DetectorRegistry struct {
+	detectors []Detector
+}
+
+func (r *DetectorRegistry) Registry(d Detector) {
+	r.detectors = append(r.detectors, d)
+}
+
+func (r *DetectorRegistry) ProcessAll(ctx context.Context, e model.Event) []model.Alert {
+	var result []model.Alert
+	for _, d := range r.detectors {
+		alert, err := d.Process(ctx, e)
+		if err != nil {
+			log.Printf("[Detector-%s] error: %v", d.ID(), err)
+			continue
+		}
+		if alert == nil {
+			continue
+		}
+		result = append(result, *alert)
+	}
+	//log.Printf("Return result len:%v", len(result))
+	return result
+}
