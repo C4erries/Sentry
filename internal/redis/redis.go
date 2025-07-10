36ea11e5399d419804550693deb2844b4a5941d9
c4erries <c4erries@gmail.com>
2025-07-10 17:46:23 +0300

    Redis adapter

--- internal/redis/redis.go
+++ internal/redis/redis.go
@@ -9,20 +9,28 @@ import (
 
 type IntCmd = redis.IntCmd
 type BoolCmd = redis.BoolCmd
-type StringCliceCmd = redis.StringSliceCmd
+type StringCmd = redis.StringCmd
+type StatusCmd = redis.StatusCmd
+type SliceCmd = redis.SliceCmd
+type StringSliceCmd = redis.StringSliceCmd
 type Z = redis.Z
 type ZRangeBy = redis.ZRangeBy
 
 type RedisClient interface {
+	//Set(ctx context.Context, key string, value interface{}, expiration time.Duration) *StatusCmd
+	//Get(ctx context.Context, key string) *StringCmd
+	HGet(ctx context.Context, key string, field string) *StringCmd
+	HMGet(ctx context.Context, key string, fields ...string) *SliceCmd
+	HSet(ctx context.Context, key string, values ...interface{}) *IntCmd
 	SetNX(ctx context.Context, key string, value interface{}, expiration time.Duration) *BoolCmd
 	ZAdd(ctx context.Context, key string, members ...Z) *IntCmd
 	ZRemRangeByScore(ctx context.Context, key, min, max string) *IntCmd
-	ZRangeByScore(ctx context.Context, key string, opt *ZRangeBy) *StringCliceCmd
+	ZRangeByScore(ctx context.Context, key string, opt *ZRangeBy) *StringSliceCmd
 	ZCard(ctx context.Context, key string) *IntCmd
 	Expire(ctx context.Context, key string, expiration time.Duration) *BoolCmd
+	Exists(ctx context.Context, keys ...string) *IntCmd
 }
 
 func add() {
-	var _ RedisClient = (*redis.Client)(nil)
 	redis.NewClient(&redis.Options{})
 }
