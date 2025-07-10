aafb6840a755e9fa48fec37aaa5ef361dfabc259
c4erries <c4erries@gmail.com>
2025-07-10 17:47:58 +0300

    Update cmd_sink

--- internal/alert/cmd_sink.go
+++ internal/alert/cmd_sink.go
@@ -10,11 +10,33 @@ import (
 type CmdSink struct {
 }
 
-func (s *CmdSink) Send(ctx context.Context, alert model.Alert) error {
+func NewCmdSink() *CmdSink {
+	return &CmdSink{}
+}
+
+type Printable interface {
+	String() string
+	IsPrintable() bool
+}
+
+func (s *CmdSink) Send(ctx context.Context, alert *model.Alert) error {
 	events_string := ""
 	for _, event := range alert.Events {
 		events_string += event.BaseEvent.EventType.String() + " "
 	}
 	log.Printf("ALERT | Rule: %s, Level: %s, DetectedAt: %s, Events: %s", alert.Rule, alert.Level, alert.DetectedAt.UTC().String(), events_string)
+	data, ok := alert.Data.(Printable)
+	if !ok {
+		return nil
+	}
+	if data.IsPrintable() {
+		log.Printf("####Data####")
+		log.Printf("%s", data.String())
+		log.Printf("############")
+	}
 	return nil
+}
+
+func (s *CmdSink) ID() string {
+	return "cmd_sink"
 }
